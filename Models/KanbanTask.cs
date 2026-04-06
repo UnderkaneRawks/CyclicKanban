5c122e2793f6eeeac89abf3c3d9cad3c37592934
UnderkaneRawks <biswajeethembram121@gmail.com>
2026-04-07 00:53:51 +0530

    Added Task and subtask data

---
+++ Models/KanbanTask.cs
@@ -0,0 +1,9 @@
+﻿public class KanbanTask
+{
+    public string ProjectName { get; set; }
+    public string Category { get; set; }
+    public string TaskName { get; set; }
+
+    // An Array strictly limited to 7 slots
+    public SubTask[] SubTasks { get; set; } = new SubTask[7];
+}
---
+++ Models/SubTask.cs
@@ -0,0 +1,11 @@
+﻿public class SubTask
+{
+    public string Name { get; set; }
+    public bool IsDone { get; set; }
+
+    // The '?' makes the TimeSpan nullable, meaning the user doesn't HAVE to set a timer.
+    public TimeSpan? TargetTime { get; set; }
+    public TimeSpan ActualTime { get; set; }
+
+    public string Notes { get; set; }
+}
