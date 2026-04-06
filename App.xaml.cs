dc98e646d884629d1f303a860eb4328c59ab269c
UnderkaneRawks <biswajeethembram121@gmail.com>
2026-04-07 00:13:02 +0530

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CyclicKanban
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
